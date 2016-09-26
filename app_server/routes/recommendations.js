0ff2dc1b2c4a5197bec114870db3dc43c8bd5d7f
Kasper <kasper.stenroos@metropolia.fi>
2016-09-26 15:03:00 +0300

    Moved recomCtrl  file, added view and something

--- app_server/routes/recommendations.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var ctrlRecom = require('../controllers/recom.controller');
-
-/* GET recommendations. */
-router.get('/', ctrlRecom.recom);
-
-module.exports = router;
