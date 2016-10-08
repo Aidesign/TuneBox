98e0a325be13f130210a906b56b7199c70da3a1f
Kasper <kasper.stenroos@metropolia.fi>
2016-10-08 19:45:04 +0300

    Changed upload folder

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(multipart({ uploadDir: 'public/uploads' }));
+app.use(multipart({ uploadDir: 'public/images' }));
 
 app.use(passport.initialize());
 
