992f31ea5a8d91c202f61cd634826f179ca04c5c
Henks <henri.riisalo@metropolia.fi>
2016-10-11 14:45:44 +0300

    Fixes to shuffle

--- app_client/controllers/room.controller.js
+++ app_client/controllers/room.controller.js
@@ -135,10 +135,13 @@
 
 		function changeDBVideo(video) {
 			if ($scope.isadmin) {
+				console.log("Change");
 				roomService.changeVideo($routeParams.roomid, video).success(function(data) {
 
 					sukka.emit('changeVideo', $routeParams.roomid);
 				});
+			} else {
+				console.log("Nada");
 			}
 		}
 
@@ -165,7 +168,6 @@
 		function randomizedVideo() {
 			if ($scope.isAdmin) {
 
-
 				var ranNum = Math.floor((Math.random() * $scope.room.tags.length) + 1);
 				var searchString = $scope.room.tags[ranNum - 1];
 				console.log(searchString);
@@ -173,7 +175,6 @@
 				var pubAfter = new Date();
 				var currentYear = pubAfter.getFullYear();
 				pubAfter.setFullYear(currentYear - 2);
-				console.log(pubAfter);
 
 				var results = [];
 
@@ -203,49 +204,7 @@
 					.error(function() {
 						$log.info('Search error');
 					});
-				if (authentication.getUserObject()._id == $scope.admin._id) {
-
-					var ranNum = Math.floor((Math.random() * $scope.room.tags.length) + 1);
-					var searchString = $scope.room.tags[ranNum - 1];
-					console.log(searchString);
-
-					var pubAfter = new Date();
-					var currentYear = pubAfter.getFullYear();
-					pubAfter.setFullYear(currentYear - 2);
-					console.log(pubAfter);
-
-					var results = [];
-
-					$http.get('https://www.googleapis.com/youtube/v3/search', {
-							params: {
-								key: 'AIzaSyBJmqwVRUJUXd2QZD1agSvI0B5DzYbiKuc',
-								type: 'video',
-								publishedAfter: pubAfter,
-								maxResults: '50',
-								part: 'id,snippet',
-								q: "'" + searchString + "' song -'the best' -'vs'"
-							}
-						})
-						.success(function(data) {
-							if (data.items.length === 0) {
-								console.log("No results");
-								return;
-							}
-							console.log(data);
-							var videoNum = Math.floor((Math.random() * data.items.length) + 0);
-							var video = {};
-							video.id = data.items[videoNum].id.videoId;
-							video.title = data.items[videoNum].snippet.title;
-							$scope.launch(video, true);
-
-						})
-						.error(function() {
-							$log.info('Search error');
-						});
-				} else {
-					console.log("BACK OFF YO");
-				}
-
+				
 			}
 		}
 
